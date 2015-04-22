7f26f5f83917359bfa91948baff1481ee344a730
Dustin Replogle <dreplogle@Dustins-MacBook-Pro.local>
2015-04-22 16:20:30 -0500

    Removed for heroku

--- quotes_website/settings.py
+++ quotes_website/settings.py
@@ -100,9 +100,9 @@ DATABASES = {
 }
 
     
-DATABASES['default'] = dj_database_url.config()
+#DATABASES['default'] = dj_database_url.config()
 # Enable Connection Pooling
-DATABASES['default']['ENGINE'] = 'django_postgrespool'
+#DATABASES['default']['ENGINE'] = 'django_postgrespool'
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
